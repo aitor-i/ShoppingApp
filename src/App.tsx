e80ce5b3bcb3a82e16a068ba3b688bd132ce4b3d
Ibarra <aitoribarra@tutanota.com>
2021-12-17 02:26:50 +0100

    ADD - fetch store data

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,23 @@
-import { Fragment } from "react";
+import { Fragment, useState, useEffect } from "react";
+
+interface StoreProductsType {
+  id: number;
+  category: string;
+  description: string;
+  image: string;
+  amount: number;
+}
+const getStoreProducts = async (): Promise<StoreProductsType[]> => {
+  return await (await fetch("https://fakestoreapi.com/products")).json();
+};
 
 function App() {
+  const [storeProducts, setStoreProducts] = useState<StoreProductsType[]>([]);
+  useEffect(() => {
+    getStoreProducts().then((data) => setStoreProducts(data));
+  }, []);
+
+  console.log(storeProducts);
   return <Fragment></Fragment>;
 }
 
