ee1d8a705febda390a74abed153f85309c58258c
Ibarra <aitoribarra@tutanota.com>
2022-01-07 12:42:22 +0100

    UPDATE - CartProducts

--- src/ui/components/CartProduct/CartProduct.tsx
+++ src/ui/components/CartProduct/CartProduct.tsx
@@ -5,8 +5,52 @@ import { StoreProductsType } from "../../../core/domain/StoreProductsType";
 
 import { Wrapper } from "./CartProduct.styles";
 
-const CartProduct: React.FC = () => {
-  return <Fragment>Cart Product</Fragment>;
+interface Props {
+  product: StoreProductsType;
+  addToCart: (clickedProduct: StoreProductsType) => void;
+  removeFromCart: (id: number) => void;
+}
+
+const CartProduct: React.FC<Props> = ({
+  product,
+  addToCart,
+  removeFromCart,
+}) => {
+  return (
+    <Fragment>
+      <Wrapper>
+        <div>
+          <h3>{product.title}</h3>
+          <div className="information">
+            <p>Price: ${product.price}</p>
+            <p>Total: ${(product.amount * product.price).toFixed(2)}</p>
+          </div>
+          <div className="buttons">
+            <Button
+              size="small"
+              disableElevation
+              variant="contained"
+              onClick={() => removeFromCart(product.id)}
+            >
+              {" "}
+              -{" "}
+            </Button>
+            <Button
+              size="small"
+              disableElevation
+              variant="contained"
+              onClick={() => addToCart(product)}
+            >
+              {" "}
+              +{" "}
+            </Button>
+          </div>
+        </div>
+
+        <img src={product.image} alt={product.title} />
+      </Wrapper>
+    </Fragment>
+  );
 };
 
 export default CartProduct;
