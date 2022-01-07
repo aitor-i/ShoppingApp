c36bcfb7510a60ba2096829439eeb20dd4a2315f
Ibarra <aitoribarra@tutanota.com>
2022-01-07 12:05:32 +0100

    ADD - CartProduct

---
+++ src/ui/components/CartProduct/CartProduct.tsx
@@ -0,0 +1,12 @@
+import Button from "@material-ui/core/Button";
+import { Fragment } from "react";
+
+import { StoreProductsType } from "../../../core/domain/StoreProductsType";
+
+import { Wrapper } from "./CartProduct.styles";
+
+const CartProduct: React.FC = () => {
+  return <Fragment>Cart Product</Fragment>;
+};
+
+export default CartProduct;
